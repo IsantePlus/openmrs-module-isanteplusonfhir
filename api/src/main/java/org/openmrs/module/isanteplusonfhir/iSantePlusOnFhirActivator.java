2449d45d4e8ba351bda6676677f5bc6e34d2118a
Piotr Mankowski <pmanko@uw.edu>
2022-03-01 12:07:15 -0800

    Cleanup

--- api/src/main/java/org/openmrs/module/isanteplusonfhir/iSantePlusOnFhirActivator.java
+++ api/src/main/java/org/openmrs/module/isanteplusonfhir/iSantePlusOnFhirActivator.java
@@ -7,7 +7,7 @@
  * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
  * graphic logo is a trademark of OpenMRS Inc.
  */
-package org.openmrs.module.labonfhir;
+package org.openmrs.module.isanteplusonfhir;
 
 import lombok.SneakyThrows;
 import org.openmrs.module.BaseModuleActivator;
